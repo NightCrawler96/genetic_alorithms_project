aa309b08b917df4ef30865715043b09c2a9f9b97
Grzegorz Jaworski <g.jaworski1996@gmail.com>
2019-01-09 20:08:30 +0100

    quick fix

--- genetic_alghorithm/helper_functions.py
+++ genetic_alghorithm/helper_functions.py
@@ -2,18 +2,26 @@ import numpy as np
 import string
 
 
+def letters():
+    alphabet = string.ascii_uppercase.replace('V', "")
+    return alphabet
+
+
 def change2coordinates(node):
     letter = node[0]
     letter = letter.capitalize()
+    alphabet = letters()
     x = float(
-        string.ascii_uppercase.index(
+        alphabet.index(
             letter
         )
     )
     y = float(node[1])
     return np.array([x, y])
 
+
 def change2name(x, y):
-    x_s = string.ascii_uppercase[int(x)]
+    alphabet = letters()
+    x_s = alphabet[int(x)]
     y_s = str(int(y))
     return x_s + y_s
